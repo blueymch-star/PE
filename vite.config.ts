f893b3bb9b87ade449c749b295e256c066c66cd8
blueymch-star <blueymch@gmail.com>
2026-03-31 12:28:18 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
-
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './', // 確保在 GitHub Pages 上能正確抓到相對路徑
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
